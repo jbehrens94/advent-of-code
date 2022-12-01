f9b68e79a5a081f1cdb333cbe552a1ed49dea1dc
Johann Behrens <me@jbehrens.nu>
2022-12-01 14:46:26 +0100

    Clean-up code

--- Sources/TwentyTwoKit/Day1.swift
+++ Sources/TwentyTwoKit/Day1.swift
@@ -1,44 +1,33 @@
 import Foundation
 
-public struct Day1 {
+public final class Day1 {
     public let inputURL = Bundle.module.url(forResource: "Input/day1", withExtension: "txt")!
+    
+    private var input: [[Int]] {
+        get throws {
+            let inputString = try String(contentsOf: inputURL)
+            return inputString
+                .split(separator: "\n\n")
+                .map { $0.components(separatedBy: "\n") }
+                .map { $0.compactMap(Int.init) }
+        }
+    }
     
     public init() {}
 }
 
 public extension Day1 {
     func partOne() throws -> Int {
-        let inputString = try String(contentsOf: inputURL)
-        let input: [[Int]] = inputString
-            .split(separator: "\n\n")
-            .map { string in
-                string
-                    .components(separatedBy: "\n")
-                    .compactMap(Int.init)
-            }
-        
-        let highest = input.map { elf in
-            return elf.reduce(0, +)
-        }
-            .max()!
-        
-        return highest
+        return try input
+            .map { $0.reduce(0, +) }
+            .sorted(by: >)
+            .first!
     }
     
     func partTwo() throws -> Int {
-        let inputString = try String(contentsOf: inputURL)
-        let input: [[Int]] = inputString
-            .components(separatedBy: "\n\n")
-            .map { string in
-                string
-                    .components(separatedBy: "\n")
-                    .compactMap(Int.init)
-            }
-        
-        let sorted = input
+        return try input
             .map { $0.reduce(0, +) }
             .sorted(by: { $0 > $1 })
-        
-        return sorted.prefix(3).reduce(0, +)
+            .prefix(3).reduce(0, +)
     }
 }
