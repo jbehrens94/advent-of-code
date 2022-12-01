7745a42f6bcbbefe5289edb8d7c2f50687dddf66
Johann Behrens <me@jbehrens.nu>
2022-12-01 14:39:24 +0100

    Solve day 2

--- Sources/Advent2022/main.swift
+++ Sources/Advent2022/main.swift
@@ -7,7 +7,7 @@ switch commands {
 case ["day1", "part1"]:
     print(try! Day1().partOne())
 case ["day1", "part2"]:
-    print("TODO.")
+    print(try! Day1().partTwo())
     
 case []:
     print("No subcommand given"); exit(1)
--- Sources/TwentyTwoKit/Day1.swift
+++ Sources/TwentyTwoKit/Day1.swift
@@ -20,8 +20,25 @@ public extension Day1 {
         let highest = input.map { elf in
             return elf.reduce(0, +)
         }
-        .max()!
+            .max()!
         
         return highest
+    }
+    
+    func partTwo() throws -> Int {
+        let inputString = try String(contentsOf: inputURL)
+        let input: [[Int]] = inputString
+            .components(separatedBy: "\n\n")
+            .map { string in
+                string
+                    .components(separatedBy: "\n")
+                    .compactMap(Int.init)
+            }
+        
+        let sorted = input
+            .map { $0.reduce(0, +) }
+            .sorted(by: { $0 > $1 })
+        
+        return sorted.prefix(3).reduce(0, +)
     }
 }
