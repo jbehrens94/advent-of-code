db6943a3e1f2c50600ca346bfd98870a83eed60c
Johann Behrens <me@jbehrens.nu>
2022-12-04 17:00:08 +0100

    Day1, part1 :)

--- Sources/Advent2021/main.swift
+++ Sources/Advent2021/main.swift
@@ -8,7 +8,6 @@ case ["day1", "part1"]:
     print(try! Day1().partOne())
 case ["day1", "part2"]:
     print(try! Day1().partTwo())
-}
     
 case []:
     print("No subcommand given"); exit(1)
--- Sources/TwentyOneKit/Day1.swift
+++ Sources/TwentyOneKit/Day1.swift
@@ -3,15 +3,42 @@ import Foundation
 public final class Day1 {
     public let inputURL = Bundle.module.url(forResource: "Input/day1", withExtension: "txt")!
     
+    public var input: [Int] {
+        get throws {
+            return try String(contentsOf: inputURL)
+                .split(separator: "\n")
+                .map(String.init)
+                .compactMap(Int.init)
+        }
+    }
+    
     public init() {}
 }
 
 public extension Day1 {
     func partOne() throws -> Int {
-        return 0
+        let inputs = try input.dropFirst()
+        let result: [ComparisonResult] = try inputs
+            .enumerated()
+            .map { index, element in
+                let previous = try input[index]
+                let result: ComparisonResult = element > previous ?
+                    .higher : .lower
+                
+                return result
+            }
+            .filter { $0 == .higher }
+        
+        return result.count
     }
     
     func partTwo() throws -> Int {
         return 0
+    }
+}
+
+extension Day1 {
+    enum ComparisonResult {
+        case lower, higher
     }
 }
