4f8829962c6bcb7d6f2b3db4e01a8fdc8cb6ecdf
Johann Behrens <me@jbehrens.nu>
2022-12-04 16:16:03 +0100

    Add Day 1 scaffold

---
+++ Sources/TwentyOneKit/Day1.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public final class Day1 {
+    public let inputURL = Bundle.module.url(forResource: "Input/day1", withExtension: "txt")!
+    
+    public init() {}
+}
+
+public extension Day1 {
+    func partOne() throws -> Int {
+        return 0
+    }
+    
+    func partTwo() throws -> Int {
+        return 0
+    }
+}
