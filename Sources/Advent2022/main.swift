945ad991e6bdb6c85d6f53da6875a0bb11a917f3
Johann Behrens <me@jbehrens.nu>
2022-12-04 13:32:22 +0100

    [2022]: Day 3 (#4)
    [2022]: Day 3 :)

--- Sources/Advent2022/main.swift
+++ Sources/Advent2022/main.swift
@@ -12,6 +12,10 @@ case ["day2", "part1"]:
     print(try! Day2().partOne())
 case ["day2", "part2"]:
     print(try! Day2().partTwo())
+case ["day3", "part1"]:
+    print(try! Day3().partOne())
+case ["day3", "part2"]:
+    print(try! Day3().partTwo())
     
 case []:
     print("No subcommand given"); exit(1)
